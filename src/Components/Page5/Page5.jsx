b6f28e67dfb5b86cb1ea943700e52cbf3549a1ec
majid598 <143610202+majid598@users.noreply.github.com>
2024-03-25 12:12:00 +0630

    updated

--- src/Components/Page5/Page5.jsx
+++ src/Components/Page5/Page5.jsx
@@ -1,16 +1,16 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import './Page5.css';
+import "./Page.css";
 
 // import required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Pagination, Navigation } from "swiper/modules";
 
 export default function App() {
   const [swiperRef, setSwiperRef] = useState(null);
@@ -20,94 +20,95 @@ export default function App() {
 
   const prepend2 = () => {
     swiperRef.prependSlide([
-      '<div class="swiper-slide">Slide ' + --prependNumber + '</div>',
-      '<div class="swiper-slide">Slide ' + --prependNumber + '</div>',
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
     ]);
   };
 
   const prepend = () => {
     swiperRef.prependSlide(
-      '<div class="swiper-slide">Slide ' + --prependNumber + '</div>'
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
     );
   };
 
   const append = () => {
     swiperRef.appendSlide(
-      '<div class="swiper-slide">Slide ' + ++appendNumber + '</div>'
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
     );
   };
 
   const append2 = () => {
     swiperRef.appendSlide([
-      '<div class="swiper-slide">Slide ' + ++appendNumber + '</div>',
-      '<div class="swiper-slide">Slide ' + ++appendNumber + '</div>',
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
     ]);
   };
 
   return (
-    <div className='page5'>
+    <div className="page5">
       <Swiper
         onSwiper={setSwiperRef}
         slidesPerView={3}
         centeredSlides={true}
         spaceBetween={30}
         pagination={{
-          type: 'fraction',
+          type: "fraction",
         }}
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-                 <a
-              href=""
-              download=""
-              className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
-            >
-              Get Code
-            </a>
-           <img src="./assets/net.jpg" alt="" />
+          <a
+            href=""
+            download=""
+            className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
+          >
+            Get Code
+          </a>
+          <img src="./assets/net.jpg" alt="" />
         </SwiperSlide>
         <SwiperSlide>
-//             <a
-              href=""
-              download=""
-              className="px-5 py-3 text-black rounded-full border-[1px] border-black font-bold hover:bg-black/70 hover:text-white hover:scale-90 transition-all duration-300 absolute bottom-1/3"
-            >
-              Get Code
-            </a>
-            <img src="./assets/my-web.png" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <a
-              href=""
-              download=""
-              className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
-            >
-              Get Code
-            </a>
-            <img src="./assets/insta.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <a
-              href=""
-              download=""
-              className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
-            >
-              Get Code
-            </a>
-            <img src="./assets/spotify.jpg" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <a
-              href=""
-              download=""
-              className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
-            >
-              Get Code
-            </a>
-            <img src="./assets/net.jpg" alt="" />
-          </SwiperSlide>
+          //{" "}
+          <a
+            href=""
+            download=""
+            className="px-5 py-3 text-black rounded-full border-[1px] border-black font-bold hover:bg-black/70 hover:text-white hover:scale-90 transition-all duration-300 absolute bottom-1/3"
+          >
+            Get Code
+          </a>
+          <img src="./assets/my-web.png" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <a
+            href=""
+            download=""
+            className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
+          >
+            Get Code
+          </a>
+          <img src="./assets/insta.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <a
+            href=""
+            download=""
+            className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
+          >
+            Get Code
+          </a>
+          <img src="./assets/spotify.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <a
+            href=""
+            download=""
+            className="px-5 py-3 text-white rounded-full border-[1px] border-white font-bold hover:bg-white/70 hover:text-black hover:scale-90 transition-all duration-300 absolute bottom-1/3"
+          >
+            Get Code
+          </a>
+          <img src="./assets/net.jpg" alt="" />
+        </SwiperSlide>
       </Swiper>
     </div>
   );
